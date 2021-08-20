6e70d5a2c69853d3e0019563ba036d6f95d7b853
Jared Melnyk <jaredmelnyk@Jareds-MacBook-Air.local>
2021-08-20 14:08:37 -0500

    seed file stuff

---
+++ script/seedData.js
@@ -0,0 +1,50 @@
+export const users = [
+  {
+    username: 'cody@gmail.com',
+    password: 'cody123',
+    paceMinutes: 8,
+    paceSeconds: 30,
+    runningStyle: 'HOBBY',
+  },
+  {
+    username: 'sarah@gmail.com',
+    password: 'sarah123',
+    paceMinutes: 7,
+    paceSeconds: 45,
+    runningStyle: 'HOBBY',
+  },
+  {
+    username: 'nicole@gmail.com',
+    password: 'nicole123',
+    paceMinutes: 8,
+    paceSeconds: 0,
+    runningStyle: 'HOBBY',
+  },
+  {
+    username: 'matt@gmail.com',
+    password: 'matt123',
+    paceMinutes: 9,
+    paceSeconds: 30,
+    runningStyle: 'HOBBY',
+  },
+  {
+    username: 'gary@gmail.com',
+    password: 'gary123',
+    paceMinutes: 10,
+    paceSeconds: 30,
+    runningStyle: 'HOBBY',
+  },
+  {
+    username: 'meredith@gmail.com',
+    password: 'meredith123',
+    paceMinutes: 6,
+    paceSeconds: 15,
+    runningStyle: 'HOBBY',
+  },
+];
+
+export const runs = [
+  {
+    date: '2018-10-18T06:45:38.000Z',
+  },
+];
