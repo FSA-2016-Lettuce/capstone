b362b941c45ceff8edf9473145692ef2f38d9451
Scott Scheine <scott.scheine@gmail.com>
2021-08-20 13:01:21 -0400

    live map on homepage with demo data

--- client/App.js
+++ client/App.js
@@ -1,15 +1,33 @@
-import React from "react";
-
-import Navbar from "./components/Navbar";
-import Drawer from "./components/Drawer";
-import Routes from "./Routes";
-import theme from "./theme";
-import { ThemeProvider } from "@material-ui/styles";
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Navbar from './components/Navbar';
+import Drawer from './components/Drawer';
+import Routes from './Routes';
+import theme from './theme';
+import { ThemeProvider } from '@material-ui/styles';
+import HomeMap from './components/HomeMap';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Drawer />
+      {/* <HomeMap /> */}
+      <MapContainer
+        style={{ height: '500px' }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </ThemeProvider>
   );
 };
---
+++ client/components/HomeMap.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import '../../public/style.css';
+
+// const dummyData = [
+//   {
+//     id: 1,
+//     coordinates: [40.759253, -73.774953],
+//     name: 'First Point',
+//   },
+//   {
+//     id: 2,
+//     coordinates: [40.779, -73.68],
+//     name: 'Second Point',
+//   },
+//   {
+//     id: 3,
+//     coordinates: [40.779, -73.68],
+//     name: 'Third Point',
+//   },
+// ];
+
+export default function HomeMap() {
+  //   return (
+  //     <MapContainer center={[40.759253, -73.774953]} zoom={12}>
+  //       <TileLayer
+  //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  //         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  //       />
+  //       {/* {dummyData.map((point) => (
+  //         <>
+  //           <Marker key={point.id} position={point.coordinates} />
+  //           <Popup key={point.id} position={point.coordinates}>
+  //             <div>
+  //               <h2>{point.name}</h2>
+  //             </div>
+  //           </Popup>
+  //         </>
+  //       ))} */}
+  //     </MapContainer>
+  //   );
+  // }
+
+  // const position = [51.505, -0.09]
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+//   var mymap = L.map('mapid').setView([40.759253, -73.774953], 7.5);
+
+//   L.tileLayer(
+//     'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png',
+//     {
+//       maxZoom: 18,
+//       attribution:
+//         'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+//         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//       id: 'mapbox/streets-v11',
+//       tileSize: 512,
+//       zoomOffset: -1,
+//     }
+//   ).addTo(mymap);
+
+//   L.marker([51.5, -0.09])
+//     .addTo(mymap)
+//     .bindPopup('<b>Hello world!</b><br />I am a popup.');
+//   // .openPopup();
+//   //the below code allows us to customize a pin (should we decide to go with a circle shaped pin-drop)
+
+//   L.circle([40.759253, -73.774953], 500, {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//   })
+//     .addTo(mymap)
+//     .bindPopup('What up lettuce!')
+
+//     // L.polygon([
+//     //   [40.459253, -73.774953],
+//     //   [40.759253, -73.774953],
+//     //   [40.759253, -74.774953],
+//     // ])
+//     .addTo(mymap)
+//     .bindPopup('I am a polygon.');
+
+//   var popup = L.popup();
+
+//   L.marker([40.789253, -73.772953])
+//     .addTo(mymap)
+//     .bindPopup('Team Lettuce is running these streets!');
+
+// }
