c471b2894015968ec49e204a072af7dd9e0bd188
Jared Melnyk <jaredmelnyk@Jareds-MacBook-Air.local>
2021-08-27 23:53:10 -0500

    remove dummy data

--- client/components/HomeMapRunView.js
+++ client/components/HomeMapRunView.js
@@ -4,33 +4,6 @@ import { Marker, Popup, useMap } from 'react-leaflet';
 import PopupData from './PopupData';
 import { _getRuns } from '../store/run';
 
-const dummyData = [
-  {
-    id: 1,
-    coordinates: [40.759253, -73.774953],
-    name: 'First Point',
-    distance: '1 mile',
-    pace: '8 min/mile',
-    date: '08/31/2021 @ 8:00AM',
-  },
-  {
-    id: 2,
-    coordinates: [40.779, -73.68],
-    name: 'Second Point',
-    distance: '2 miles',
-    pace: '12 min/mile',
-    date: '08/31/2021 @ 10:00AM',
-  },
-  {
-    id: 3,
-    coordinates: [40.76, -73.6],
-    name: 'Third Point',
-    distance: '10 miles',
-    pace: '6 min/mile',
-    date: '08/31/2021 @ 8:00PM',
-  },
-];
-
 const HomeMapRunView = (props) => {
   console.log('HomeMapRunView props: ', props);
   const dispatch = useDispatch();
