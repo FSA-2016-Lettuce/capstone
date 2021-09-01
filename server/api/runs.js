667b651b7bf0c40ba4b2b176be7a7e6b47f6c8be
Scott Scheine <scott.scheine@gmail.com>
2021-08-31 21:01:28 -0400

    filtered runs backend finalized

--- server/api/runs.js
+++ server/api/runs.js
@@ -24,49 +24,30 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
+// Filtered GET all runs
 router.get('/', async (req, res, next) => {
   try {
-    console.log('ARE WE GETTING REQ.QUERY?', req.query);
     let { pace, distance, runStart } = req.query;
 
-    // console.log('MESSING WITH MOMENT:', moment(runStart).add(10, 'hours'));
-    // console.log('MESSING WITH MOMENT:', moment(runStart).subtract(1, 'hours'));
+    // Convert pace and distance to numbers
+    pace = Number(pace);
+    distance = Number(distance);
 
-    if (pace !== '0') {
-      pace = Number(pace);
-    }
-    if (distance !== '0') {
-      distance = Number(distance);
-    }
-
+    // Create containers for Run and Route where clauses inside of Run.findAll
     const whereClauseRun = {};
     const whereClauseRoute = {};
 
+    // Set up where clauses
     if (pace > 0 && pace < 1000) {
       whereClauseRun.pace = {
-        [Op.between]: [pace - 61, pace + 61],
+        [Op.between]: [pace - 60, pace + 60],
       };
-
-      // whereClauseRun[Op.and] = [
-      //   {
-      //     pace: {
-      //       [Op.between]: [pace - 61, pace + 61],
-      //     },
-      //   },
-      // ];
     }
 
     if (pace === 10000) {
       whereClauseRun.pace = {
         [Op.gte]: 12 * 60,
       };
-      // whereClauseRun[Op.and] = [
-      //   {
-      //     pace: {
-      //       [Op.gte]: 12 * 60,
-      //     },
-      //   },
-      // ];
     }
 
     whereClauseRun.startDate = {
@@ -76,20 +57,9 @@ router.get('/', async (req, res, next) => {
       ],
     };
 
-    // whereClauseRun[Op.and] = [
-    //   {
-    //     startDate: {
-    //       [Op.between]: [
-    //         moment(runStart).subtract(1, 'hours'),
-    //         moment(runStart).add(1, 'hours'),
-    //       ],
-    //     },
-    //   },
-    // ];
-
     if (distance > 0 && distance < 10000 * 5280) {
       whereClauseRoute.distance = {
-        [Op.between]: [distance - 5281, distance + 5281],
+        [Op.between]: [distance - 5280, distance + 5280],
       };
     }
 
@@ -99,6 +69,7 @@ router.get('/', async (req, res, next) => {
       };
     }
 
+    // Find all runs with associated filters
     const runs = await Run.findAll({
       where: whereClauseRun,
       include: [
@@ -111,32 +82,7 @@ router.get('/', async (req, res, next) => {
       ],
       order: [[Route, Waypoint, 'pathIndex', 'ASC']],
     });
-    // console.log('runs from server:', runs);
     res.json(runs);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/', async (req, res, next) => {
-  try {
-    // grab user home location from query parameters
-    const homeLat = req.query.lat;
-    const homeLng = req.query.lng;
-    // get all routes from database
-    const routes = await Route.findAll({
-      include: [Waypoint],
-      order: [[Waypoint, 'pathIndex', 'ASC']],
-    });
-    // filter routes to only those with starting point within 5 miles of user home location
-    const localRoutes = routes.filter((route) => {
-      const distanceFromHome = pointToPointDistance(
-        [homeLat, homeLng],
-        [route.waypoints[0].latitude, route.waypoints[0].longitude]
-      );
-      return distanceFromHome <= 26400;
-    });
-    res.json(localRoutes);
   } catch (err) {
     next(err);
   }
