22e3bd49e5ddf81acf2edf3b2026c560d690b8cb
ChrisNieves <nievescs20@gmail.com>
2021-09-02 15:00:33 -0400

    made corrections to profEdit and userProf

--- client/components/UserProfileForm.js
+++ client/components/UserProfileForm.js
@@ -7,6 +7,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateUserThunk } from '../store/auth';
@@ -23,13 +24,17 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     margin: theme.spacing(1),
+    width: '60%',
   },
   container: {
     marginTop: '15px',
   },
   formControl: {
     margin: theme.spacing(1),
-    width: '100%',
+    width: '92%',
+  },
+  title: {
+    margin: '8px',
   },
 }));
 
@@ -77,113 +82,118 @@ export default function UserProfileForm() {
 
   return (
     <Container maxWidth="sm">
+      <Typography variant="h5" className={classes.title}>
+        Edit Profile
+      </Typography>
       <Box display="flex" justifyContent="space-around" flexWrap="wrap">
-        <form className={classes.root} noValidate autoComplete="off">
-          <TextField
-            required
-            name="firstName"
-            label="First Name"
-            defaultValue={user.firstName}
-            variant="outlined"
-            onChange={changeHandler}
-          />
-          <TextField
-            required
-            name="lastName"
-            label="Last Name"
-            defaultValue={user.lastName}
-            variant="outlined"
-            onChange={changeHandler}
-          />
-          <TextField
-            required
-            name="password"
-            label="Password"
-            defaultValue="***"
-            type="password"
-            variant="outlined"
-            onChange={changeHandler}
-          />
-          <TextField
-            name="address"
-            label="Address"
-            defaultValue={user.address ? user.address : ''}
-            variant="outlined"
-            onChange={changeHandler}
-          />
-          <FormControl variant="outlined" className={classes.formControl}>
-            <InputLabel id="demo-simple-select-outlined-label">
-              Preffered Pace
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-outlined-label"
-              value={formState.pace}
+        <Container maxWidth="sm">
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+              required
+              name="firstName"
+              label="First Name"
+              defaultValue={user.firstName}
+              variant="outlined"
               onChange={changeHandler}
-              label="pace"
-              name="pace"
-            >
-              <MenuItem value={0}>
-                <em>All</em>
-              </MenuItem>
-              <MenuItem value={240}>4:00 min/mi</MenuItem>
-              <MenuItem value={270}>4:30 min/mi</MenuItem>
-              <MenuItem value={300}>5:00 min/mi</MenuItem>
-              <MenuItem value={330}>5:30 min/mi</MenuItem>
-              <MenuItem value={360}>6:00 min/mi</MenuItem>
-              <MenuItem value={390}>6:30 min/mi</MenuItem>
-              <MenuItem value={420}>7:00 min/mi</MenuItem>
-              <MenuItem value={450}>7:30 min/mi</MenuItem>
-              <MenuItem value={480}>8:00 min/mi</MenuItem>
-              <MenuItem value={510}>8:30 min/mi</MenuItem>
-              <MenuItem value={540}>9:00 min/mi</MenuItem>
-              <MenuItem value={570}>9:30 min/mi</MenuItem>
-              <MenuItem value={600}>10:00 min/mi</MenuItem>
-              <MenuItem value={630}>10:30 min/mi</MenuItem>
-              <MenuItem value={660}>11:00 min/mi</MenuItem>
-              <MenuItem value={690}>11:30 min/mi</MenuItem>
-              <MenuItem value={720}>12:00 min/mi</MenuItem>
-              <MenuItem value={10000}>12:00+ min/mi</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl variant="outlined" className={classes.formControl}>
-            <InputLabel id="demo-simple-select-outlined-label">
-              Preffered Distance
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-outlined-label"
-              value={formState.distance}
+            />
+            <TextField
+              required
+              name="lastName"
+              label="Last Name"
+              defaultValue={user.lastName}
+              variant="outlined"
               onChange={changeHandler}
-              label="distance"
-              name="distance"
-            >
-              <MenuItem value={0}>
-                <em>All</em>
-              </MenuItem>
-              <MenuItem value={1 * 5280}>1 mile</MenuItem>
-              <MenuItem value={2 * 5280}>2 miles</MenuItem>
-              <MenuItem value={3 * 5280}>3 miles</MenuItem>
-              <MenuItem value={4 * 5280}>4 miles</MenuItem>
-              <MenuItem value={5 * 5280}>5 miles</MenuItem>
-              <MenuItem value={6 * 5280}>6 miles</MenuItem>
-              <MenuItem value={7 * 5280}>7 miles</MenuItem>
-              <MenuItem value={8 * 5280}>8 miles</MenuItem>
-              <MenuItem value={9 * 5280}>9 miles</MenuItem>
-              <MenuItem value={10 * 5280}>10 miles</MenuItem>
-              <MenuItem value={11 * 5280}>11 miles</MenuItem>
-              <MenuItem value={12 * 5280}>12 miles</MenuItem>
-              <MenuItem value={10000 * 5280}>12+ miles</MenuItem>
-            </Select>
-          </FormControl>
-        </form>
+            />
+            <TextField
+              required
+              name="password"
+              label="Password"
+              defaultValue="***"
+              type="password"
+              variant="outlined"
+              onChange={changeHandler}
+            />
+            <TextField
+              name="address"
+              label="Address"
+              defaultValue={user.address ? user.address : ''}
+              variant="outlined"
+              onChange={changeHandler}
+            />
+            <FormControl variant="outlined" className={classes.formControl}>
+              <InputLabel id="demo-simple-select-outlined-label">
+                Preferred Pace
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                value={formState.pace}
+                onChange={changeHandler}
+                label="pace"
+                name="pace"
+              >
+                <MenuItem value={0}>
+                  <em>All</em>
+                </MenuItem>
+                <MenuItem value={240}>4:00 min/mi</MenuItem>
+                <MenuItem value={270}>4:30 min/mi</MenuItem>
+                <MenuItem value={300}>5:00 min/mi</MenuItem>
+                <MenuItem value={330}>5:30 min/mi</MenuItem>
+                <MenuItem value={360}>6:00 min/mi</MenuItem>
+                <MenuItem value={390}>6:30 min/mi</MenuItem>
+                <MenuItem value={420}>7:00 min/mi</MenuItem>
+                <MenuItem value={450}>7:30 min/mi</MenuItem>
+                <MenuItem value={480}>8:00 min/mi</MenuItem>
+                <MenuItem value={510}>8:30 min/mi</MenuItem>
+                <MenuItem value={540}>9:00 min/mi</MenuItem>
+                <MenuItem value={570}>9:30 min/mi</MenuItem>
+                <MenuItem value={600}>10:00 min/mi</MenuItem>
+                <MenuItem value={630}>10:30 min/mi</MenuItem>
+                <MenuItem value={660}>11:00 min/mi</MenuItem>
+                <MenuItem value={690}>11:30 min/mi</MenuItem>
+                <MenuItem value={720}>12:00 min/mi</MenuItem>
+                <MenuItem value={10000}>12:00+ min/mi</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl variant="outlined" className={classes.formControl}>
+              <InputLabel id="demo-simple-select-outlined-label">
+                Preferred Distance
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                value={formState.distance}
+                onChange={changeHandler}
+                label="distance"
+                name="distance"
+              >
+                <MenuItem value={0}>
+                  <em>All</em>
+                </MenuItem>
+                <MenuItem value={1 * 5280}>1 mile</MenuItem>
+                <MenuItem value={2 * 5280}>2 miles</MenuItem>
+                <MenuItem value={3 * 5280}>3 miles</MenuItem>
+                <MenuItem value={4 * 5280}>4 miles</MenuItem>
+                <MenuItem value={5 * 5280}>5 miles</MenuItem>
+                <MenuItem value={6 * 5280}>6 miles</MenuItem>
+                <MenuItem value={7 * 5280}>7 miles</MenuItem>
+                <MenuItem value={8 * 5280}>8 miles</MenuItem>
+                <MenuItem value={9 * 5280}>9 miles</MenuItem>
+                <MenuItem value={10 * 5280}>10 miles</MenuItem>
+                <MenuItem value={11 * 5280}>11 miles</MenuItem>
+                <MenuItem value={12 * 5280}>12 miles</MenuItem>
+                <MenuItem value={10000 * 5280}>12+ miles</MenuItem>
+              </Select>
+            </FormControl>
+          </form>
+        </Container>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={submitHandler}
+        >
+          Save Changes
+        </Button>
       </Box>
-      <Button
-        className={classes.button}
-        variant="contained"
-        color="primary"
-        onClick={submitHandler}
-      >
-        Save Changes
-      </Button>
     </Container>
   );
 }
