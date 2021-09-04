47a7908e43e3d037258b7e155a2a931bab8f3839
Jose_Fernandez <jfernand84@gmail.com>
2021-09-03 23:56:06 -0400

    dynamically rendered user stats...going to style and turn in tomorrow

--- client/components/stats/UserStatsPage.js
+++ client/components/stats/UserStatsPage.js
@@ -1,14 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import reducer, { updateUserThunk } from '../../store/auth';
 import { _getStats } from '../../store/run';
 
 export default function UserStats(props) {
   const userId = props.match.params.id;
-
-  console.log('props in UserStats: ', props);
   const user = useSelector((state) => state.auth);
-  console.log('what is user.id', user.id)
+  console.log('what is user.id', user.id);
 
   const runs = useSelector((state) => state.run.allRuns);
   console.log('runs in stats:', runs);
@@ -16,11 +14,23 @@ export default function UserStats(props) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-     async function loadStats() {
+    async function loadStats() {
       await dispatch(_getStats(userId));
     }
     loadStats();
+    console.log(runs);
   }, []);
 
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      {runs[0] && (
+        <div>
+          <h1>{runs[0].id}</h1>
+          <h1>{runs[0].startDate}</h1>
+          <h1>{runs[0].pace}</h1>
+          <h1>{runs[0].status}</h1>
+        </div>
+      )}
+    </div>
+  );
 }
