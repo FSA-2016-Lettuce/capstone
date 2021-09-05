adaa0120bc67cc5b33958484c5227227e3e788ce
Jose_Fernandez <jfernand84@gmail.com>
2021-09-05 12:32:33 -0400

    switching branches

--- server/db/models/Image.js
+++ server/db/models/Image.js
@@ -2,5 +2,12 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Image = db.define('image', {
+  //the below keys are needed to satisfy Postgres format
+    imageType: Sequelize.STRING,
+    imageName: Sequelize.STRING,
+    imageData: Sequelize.BLOB('long')
 
 })
+
+module.exports = Image;
+
